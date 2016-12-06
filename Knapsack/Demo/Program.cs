ae94056e1c576384224f892e2321643c35445307
Antonia Elek <antoniaelek@hotmail.com>
2016-12-06 21:49:01 +0100

    Increase print padding in demo app.

--- Knapsack/Demo/Program.cs
+++ Knapsack/Demo/Program.cs
@@ -9,7 +9,7 @@ namespace Demo
             var k = new Knapsack.Knapsack(
                 new Category("c1", new Element("e11", 5, 5), new Element("e12", 10, 10)),
                 new Category("c2", new Element("e21", 5, 5), new Element("e22", 10, 10)));
-            k.PrintTable(10);
+            k.PrintTable(12);
         }
     }
 }
